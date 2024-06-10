afb6708c02db030ac8edafb4c0526322d420868e
IustinDragan <iustindragan@gmail.com>
2024-06-10 16:57:55 +0300

    Refactoring for UtilityMonitorProgram NUnit Test added

---
+++ ProcessMain.cs
@@ -0,0 +1,35 @@
+﻿
+namespace UtilityMonitorForWindows
+{
+    public static class ProcessMain
+    {
+        public static async Task RunAsync(string processName, int lifeTime, int frequency)
+        {
+            while (true)
+            {
+                try
+                {
+                    var processes = ProcessesHelper.GetAllProcessesThatCanBeKilled(processName, lifeTime);
+
+                    foreach (var process in processes)
+                    {
+                        process.Kill();
+
+                        Console.WriteLine($"Successfully closed the process: {process.Id}, {process.ProcessName}");
+                    }
+
+                    Console.WriteLine($"Done for searching processes. Retrying in {frequency} minutes");
+
+                    await ProcessesHelper.CloseTheProgramIfKeyIsPressedAsync(frequency);
+
+                }
+                catch (StopProgramException) 
+                {
+                    Console.WriteLine("Exit the program");
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ StopProgramException.cs
@@ -0,0 +1,7 @@
+﻿namespace UtilityMonitorForWindows
+{
+    public class StopProgramException : Exception
+    {
+        public StopProgramException() : base("The program has been stopped by the user.") { }
+    }
+}
